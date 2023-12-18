9b84341473e20e6e7ccb62ce80ca3bd21c20e0bc
y-anishchenko <programm85@gmail.com>
2023-12-18 18:27:05 +0600

    Added changes

---
+++ scripts/menu-toggle.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('show');
+    });
